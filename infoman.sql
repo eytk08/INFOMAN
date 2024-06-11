8a8007136295b7521c1f62675efd00f163ad4e22
David Garcia <145320700+Davidg13229@users.noreply.github.com>
2024-06-12 00:51:47 +0800

    Add files via upload

---
+++ infoman.sql
@@ -0,0 +1,177 @@
+CREATE DATABASE  IF NOT EXISTS `infoman` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `infoman`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: infoman
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `allergy`
+--
+
+DROP TABLE IF EXISTS `allergy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `allergy` (
+  `allergen_Code` int NOT NULL AUTO_INCREMENT, -- check nalang if yan or yung char(4) NOT NULL based sa masterlist sa excel na napagusapan -- 
+  `fk_allergy_patient_ID` int NOT NULL, 
+  `allergenName` varchar(30) DEFAULT NULL,
+  `allergenMed` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`allergen_Code`,`fk_allergy_patient_ID`),
+  KEY `fk_patient_ID_idx` (`fk_allergy_patient_ID`), 
+  CONSTRAINT `fk_allergy_patient_ID` FOREIGN KEY (`fk_allergy_patient_ID`) REFERENCES `patient` (`patient_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `allergy`
+--
+
+LOCK TABLES `allergy` WRITE;
+/*!40000 ALTER TABLE `allergy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `allergy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `condition`
+--
+
+DROP TABLE IF EXISTS `condition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `condition` (
+  `condition_ID` char(4) NOT NULL, -- not really sure kung char 4 pa ba or set ko same sa surgery_code na datatype and size since may napaguaspan abt sa masterlist -- 
+  `fk_condition_patient_ID` int NOT NULL,  
+  `conditionName` varchar(50) DEFAULT NULL,
+  `conditionDiagnosis` date DEFAULT NULL,
+  `conditionMed` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`condition_ID`,`fk_condition_patient_ID`),
+  KEY `fk_patient_ID_idx` (`fk_condition_patient_ID`),
+  CONSTRAINT `fk_condition_patient_ID` FOREIGN KEY (`fk_condition_patient_ID`) REFERENCES `patient` (`patient_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `condition`
+--
+
+LOCK TABLES `condition` WRITE;
+/*!40000 ALTER TABLE `condition` DISABLE KEYS */;
+/*!40000 ALTER TABLE `condition` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctor` (
+  `doctor_ID` int NOT NULL AUTO_INCREMENT, 
+  `doctorPDoc` varchar(40) DEFAULT NULL,
+  `doctorPNum` varchar(11) DEFAULT NULL, -- Format nalang kaagad na +63 sa frontend para ang ilalagay nalang sa start ay either 09 or 9 nalang  
+  `doctorPEmail` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`doctor_ID`),
+  UNIQUE KEY `doctorPEmail_UNIQUE` (`doctorPEmail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctor`
+--
+
+LOCK TABLES `doctor` WRITE;
+/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patient` (
+  `patient_ID` int NOT NULL AUTO_INCREMENT, -- dunno if (1,2,3,4,5,6 (Increment)) or sundin yung (104523) which will become char(6) NOT NULL -- 
+  `patientName` varchar(60) NOT NULL,
+  `patientAge` int NOT NULL,
+  `patientSex` enum('Male','Female','M','F') NOT NULL, -- not sure dito di namin magawa ng char nag iisip pa if m,f or male female --
+  `patientBday` date NOT NULL, 
+  `patientRel` varchar(30) NOT NULL, 
+  `patientMarStat` varchar(30) NOT NULL, -- not sure rin dito pwede ito gawing enum('Single','Married','Widowed,'Separated')
+  `patientBType` enum('A+','A-','B+','B-','AB+','AB-','O+','O-') DEFAULT NULL,
+  `patientHeight` decimal(3,2) NOT NULL, -- decimal siguro ito 
+  `patientWeight` decimal(3,2) NOT NULL, -- decimal siguro ito para precise and exact 
+  `patientPNum` varchar(11) NOT NULL, -- Format nalang kaagad na +63 sa frontend para ang ilalagay nalang sa start ay either 09 or 9 nalang  
+  `patientEmail` varchar(40) NOT NULL, 
+  `patientOccup` varchar(30) NOT NULL,
+  `fk_doctor_ID` int DEFAULT NULL,
+  PRIMARY KEY (`patient_ID`),
+  UNIQUE KEY `patientPNum_UNIQUE` (`patientPNum`),
+  UNIQUE KEY `patientEmail_UNIQUE` (`patientEmail`),
+  KEY `fk_doctor_ID_idx` (`fk_doctor_ID`),
+  CONSTRAINT `fk_doctor_ID` FOREIGN KEY (`fk_doctor_ID`) REFERENCES `doctor` (`doctor_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `surgery`
+--
+
+DROP TABLE IF EXISTS `surgery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `surgery` (
+  `surgery_Code` int NOT NULL AUTO_INCREMENT, 
+  `fk_surgery_patient_ID` int NOT NULL,
+  `surgeryLoc` varchar(30) DEFAULT NULL,
+  `surgeryType` varchar(20) DEFAULT NULL,
+  `surgeryYear` date DEFAULT NULL,
+  PRIMARY KEY (`surgery_Code`,`fk_surgery_patient_ID`),
+  KEY `fk_surgery_patient_ID_idx` (`fk_surgery_patient_ID`),
+  CONSTRAINT `fk_surgery_patient_ID` FOREIGN KEY (`fk_surgery_patient_ID`) REFERENCES `patient` (`patient_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `surgery`
+--
+
+LOCK TABLES `surgery` WRITE;
+/*!40000 ALTER TABLE `surgery` DISABLE KEYS */;
+/*!40000 ALTER TABLE `surgery` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-11 20:52:42
